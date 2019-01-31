3c24fdca2b6a045f759c9ac736f5d5fe22d22a34
Tomaz Muraus <tomaz@tomaz.me>
2019-01-31 13:48:26 +0100

    Use value from config.

--- actions/build_text.py
+++ actions/build_text.py
@@ -6,12 +6,14 @@ from st2common.runners.base_action import Action
 
 from lib import community
 
+
 class BuildText(Action):
     """Community Update Action
     """
     def __init__(self, config):
         super(BuildText, self).__init__(config)
         self._token = config.get('token')
+        self._forum_feed_url = config.get('forum_feed_url')
 
     def run(self, body=None, user=None, delta=None):
         """Build and return rendered text
@@ -23,9 +25,11 @@ class BuildText(Action):
             seconds=delta.get('seconds', 0)
         )
 
+        # TODO: Combine 2 rules and only retrieve forum posts once, not once per rule
         return {
             'body': community.build_text(
-                self._token,
+                token=self._token,
+                forum_feed_url=self._forum_feed_url,
                 body=body,
                 user=user,
                 delta=time_delta
